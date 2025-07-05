72346a95d168d26646278c69153c2c37cfd655ce
Huy Huynh <145945591+huy-cao-huynh@users.noreply.github.com>
2025-07-05 00:46:08 -0700

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -23,21 +23,22 @@ let determineComputedTheme = () => {
 const browserPref = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 
 // Set the theme on page load or when explicitly called
-let setTheme = (theme) => {
-  const use_theme =
-    theme ||
-    localStorage.getItem("theme") ||
-    $("html").attr("data-theme") ||
-    browserPref;
+let setTheme = ("light");
+// let setTheme = (theme) => {
+//   const use_theme =
+//     theme ||
+//     localStorage.getItem("theme") ||
+//     $("html").attr("data-theme") ||
+//     browserPref;
 
-  if (use_theme === "dark") {
-    $("html").attr("data-theme", "dark");
-    $("#theme-icon").removeClass("fa-sun").addClass("fa-moon");
-  } else if (use_theme === "light") {
-    $("html").removeAttr("data-theme");
-    $("#theme-icon").removeClass("fa-moon").addClass("fa-sun");
-  }
-};
+//   if (use_theme === "dark") {
+//     $("html").attr("data-theme", "dark");
+//     $("#theme-icon").removeClass("fa-sun").addClass("fa-moon");
+//   } else if (use_theme === "light") {
+//     $("html").removeAttr("data-theme");
+//     $("#theme-icon").removeClass("fa-moon").addClass("fa-sun");
+//   }
+// };
 
 // Toggle the theme manually
 var toggleTheme = () => {
